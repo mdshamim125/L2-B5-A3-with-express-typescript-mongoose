6e9fcfade71bcae1c5cc19fc1b6587b838a7e740
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2025-06-20 15:37:00 +0600

    added pre middleware and static method to the borrow model

--- src/modules/borrow/borrow.interface.ts
+++ src/modules/borrow/borrow.interface.ts
@@ -1,7 +1,12 @@
-import { Types } from "mongoose";
+import { Model, Types } from "mongoose";
 
 export interface IBorrow{
     book: Types.ObjectId;
     quantity: number;
     dueDate: Date;
+}
+
+
+export interface IBorrowModel extends Model<IBorrow> {
+  borrowBook(borrowData: IBorrow): Promise<IBorrow>;
 }
--- src/modules/borrow/borrow.model.ts
+++ src/modules/borrow/borrow.model.ts
@@ -1,7 +1,8 @@
 import { model, Schema } from "mongoose";
-import { IBorrow } from "./borrow.interface";
+import { IBorrow, IBorrowModel } from "./borrow.interface";
+import Book from "../book/book.model";
 
-const borrowSchema = new Schema<IBorrow>(
+const borrowSchema = new Schema<IBorrow, IBorrowModel>(
   {
     book: {
       type: Schema.Types.ObjectId,
@@ -20,5 +21,42 @@ const borrowSchema = new Schema<IBorrow>(
   }
 );
 
-const Borrow = model<IBorrow>("Borrow", borrowSchema);
+borrowSchema.pre("save", function(next){
+    if(this.dueDate < new Date()){
+        return next(new Error("Due date cannot be in the past."))
+    }
+    next();
+})
+
+
+borrowSchema.statics.borrowBook = async function (borrowData: IBorrow) {
+  const book = await Book.findById(borrowData.book);
+
+  if (!book) {
+    throw new Error("Book not found");
+  }
+
+  if (book.copies < borrowData.quantity) {
+    throw new Error("Not enough copies available");
+  }
+
+  // Deduct quantity
+  book.copies -= borrowData.quantity;
+
+  // If no more copies, mark as unavailable
+  if (book.copies === 0) {
+    book.available = false;
+  }
+
+  await book.save(); // Update book data
+
+  // Create the borrow record
+  const borrow = await this.create(borrowData);
+  return borrow;
+};
+
+const Borrow = model<IBorrow, IBorrowModel>("Borrow", borrowSchema);
 export default Borrow;
+
+// const Borrow = model<IBorrow>("Borrow", borrowSchema);
+// export default Borrow;
